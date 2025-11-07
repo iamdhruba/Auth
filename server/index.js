7210445f82a06481a4ea6e945e31a13c9e2bc00e
medhruba <dhrubarajchaudhary498@gmail.com>
2025-11-07 07:46:07 +0545

    solving frontend and backend connection issue

--- server/index.js
+++ server/index.js
@@ -13,8 +13,17 @@ import { app, server } from "./config/socket.js";
 dotenv.config();
 connectDB();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://auth-t07c.onrender.com",
+  "https://auth-bice-delta.vercel.app"
+];
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
